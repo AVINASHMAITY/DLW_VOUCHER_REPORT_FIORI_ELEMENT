ae21fc51d9be2c1d4dd12412d26931d4f606d751
Gaurav Kumar Jain <gauravkumar.jain@delaware.pro>
2026-05-29 18:43:25 +0000

    Added Action Button: for pdf Preview without code

---
+++ webapp/ext/CustomAction.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/m/MessageToast"
+], function (MessageToast) {
+    "use strict";
+
+    return {
+
+        onPreviewPDF: function (oEvent) {
+
+            MessageToast.show("Preview Button Working");
+
+        }
+
+    };
+});
